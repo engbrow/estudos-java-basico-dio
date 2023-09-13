3b6916c6a8e33d39bff9423240ea3fdcdceb6f1f
engbrow <fernandobrowse@icloud.com>
2023-09-13 15:16:35 -0300

    tipos, variaveis e operadores

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,26 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        double salarioMinimo = 2500;
+
+        //tipos primitivos
+        //estudem classes String que representa texto na aplica;ão   
+
+        String meuNome = "Fernando Soares";
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 5;
+
+        numero = 10;
+        System.out.println(numero);
+
+        final double VALOR_DE_PI = 3.14;
+
+        System.out.println(VALOR_DE_PI);
+
+
+    
     }
 }
