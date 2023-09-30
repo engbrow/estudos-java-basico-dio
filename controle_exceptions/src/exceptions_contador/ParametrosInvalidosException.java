6e0b1a92635927eddf0cfc07271a7e77e53844d5
engbrow <fernandobrowse@icloud.com>
2023-09-30 12:44:58 -0300

    commit projeto do desafio contador exceptions dio

---
+++ controle_exceptions/src/exceptions_contador/ParametrosInvalidosException.java
@@ -0,0 +1,7 @@
+package exceptions_contador;
+
+public class ParametrosInvalidosException extends Exception {
+    public ParametrosInvalidosException(String message) {
+        super(message);
+    }
+}
